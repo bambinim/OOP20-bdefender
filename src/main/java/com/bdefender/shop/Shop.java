c697c13244ca8bb740a74913d8fdcbcdb2f9fe3d
oshodiS <shola.oshodi@studio.unibo.it>
2021-04-09 19:08:32 +0200

    Refactor and fixed warning

--- src/main/java/com/bdefender/shop/Shop.java
+++ src/main/java/com/bdefender/shop/Shop.java
@@ -1,19 +1,19 @@
 package com.bdefender.shop;
+import com.bdefender.tower.Tower;
 
 import java.util.Optional;
 
 import com.bdefender.Pair;
 import com.bdefender.enemies.pool.EnemiesPoolInteractor;
-import com.bdefender.towers.TowerBase;
+
 
 public interface Shop {
     boolean hasBoughtTower(Integer price);
-    void buyUpgrade();
-	int getTowerPrice(String twrName);
-	void setTowerPrice(String twrName, Integer price);
-	Optional<TowerBase> buyTowerZone1(EnemiesPoolInteractor pool, Pair<Double, Double> pos,String twrName);
-	Optional<TowerBase> buyTowerZone2(EnemiesPoolInteractor pool, Pair<Double, Double> pos,String twrName);
-	Optional<TowerBase> buyTowerDirect1(EnemiesPoolInteractor pool, Pair<Double, Double> pos,String twrName);
-	void buyUpgrade(TowerBase tw, String twName);
+    int getTowerPrice(String twrName);
+    void setTowerPrice(String twrName, Integer price);
+    Optional<Tower> buyTowerZone1(EnemiesPoolInteractor pool, Pair<Double, Double> pos, String twrName);
+    Optional<Tower> buyTowerZone2(EnemiesPoolInteractor pool, Pair<Double, Double> pos, String twrName);
+    Optional<Tower> buyTowerDirect1(EnemiesPoolInteractor pool, Pair<Double, Double> pos, String twrName);
+    void buyUpgrade(Tower tw, String twName);
 
 }
--- src/main/java/com/bdefender/shop/ShopImpl.java
+++ src/main/java/com/bdefender/shop/ShopImpl.java
@@ -1,100 +1,84 @@
 package com.bdefender.shop;
 
+import com.bdefender.tower.Tower;
+import com.bdefender.tower.TowerFactory;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-
 import com.bdefender.Pair;
 import com.bdefender.enemies.pool.EnemiesPoolInteractor;
-import com.bdefender.towers.TowerBase;
-import com.bdefender.towers.TowerFactory;
-import com.bdefender.wallet.*;
+import com.bdefender.wallet.Wallet;
 
 public class ShopImpl implements Shop {
-	//i diversi tipi di torre dovrebbero forse avere un nome univoco o un id per distinguerle
-	Wallet wallet; //to manage the money that the user has
-	TowerFactory tf = new TowerFactory();
-	Map<String, Integer> twrPrices = new HashMap<>(); //to set prices of tower
-	Map<String, Integer> upgPrices = new HashMap<>(); //to set prices of upgrade
-	
-	public ShopImpl(Wallet wallet) {
-		this.wallet = wallet;
-	}
-	
-	/*
-	 * @return the price of a specific tower
-	 * 
-	 */
-	public int getTowerPrice(String twrName){
-		return this.twrPrices.get(twrName);
-	}
-	
-	/*
-	 * set the price of a specific tower
-	 */
-	
-	@Override
-	public void setTowerPrice(String twrName, Integer price) {
-		twrPrices.put(twrName, price);
-	}
-	/**
-	 * check if is possible to buy a specific tower
-	 * @return true if the purchase is successful  
-	 */
+    private Wallet wallet; //to manage the money that the user has
+    private TowerFactory tf = new TowerFactory();
+    private Map<String, Integer> twrPrices = new HashMap<>(); //to set prices of tower
+    private Map<String, Integer> upgPrices = new HashMap<>(); //to set prices of upgrade
 
-	@Override
-	public boolean hasBoughtTower(Integer price) {
-		if(wallet.areMoneyEnough(price)) {
-			wallet.subtractMoney(price);
-			return true;
-		}
-		return false;
-	}
-	
-	//if is possible to buy the tower then return a new tower which has to be added to the map
-	public Optional<TowerBase> buyTowerZone1(EnemiesPoolInteractor pool, Pair<Double, Double> pos,String twrName) {
-		int twrPrice = twrPrices.get(twrName);
-		return hasBoughtTower(twrPrice)? Optional.of(tf.getTowerZone1(pool, pos)): Optional.empty();
-	}
-	
-	public Optional<TowerBase> buyTowerZone2(EnemiesPoolInteractor pool, Pair<Double, Double> pos,String twrName) {
-		int twrPrice = twrPrices.get(twrName);
-		return hasBoughtTower(twrPrice)? Optional.of(tf.getTowerZone2(pool, pos)): Optional.empty();
-	}
-	
-	public Optional<TowerBase> buyTowerDirect1(EnemiesPoolInteractor pool, Pair<Double, Double> pos,String twrName) {
-		int twrPrice = twrPrices.get(twrName);
-		return hasBoughtTower(twrPrice)? Optional.of(tf.getTowerDirect1(pool, pos)): Optional.empty();
-	}
+    public ShopImpl(final Wallet wallet) {
+        this.wallet = wallet;
+    }
 
+    /*
+     * @return the price of a specific tower
+     * 
+     */
+    public final int getTowerPrice(final String twrName) {
+        return this.twrPrices.get(twrName);
+    }
 
-	
-	//sarebbe meglio inserire un editor 
-	/*
-	 * upgrade the level of the towers
-	 * */
-	@Override
-	public void buyUpgrade(TowerBase tw, String twName) {
-		Integer upPrice = upgPrices.get(twName);
-		if(wallet.areMoneyEnough(upPrice)){
-			wallet.subtractMoney(upPrice);
-			tw.upgradeLevel();
-		}
-
-	}
+    /*
+     * set the price of a specific tower
+     */
 
     @Override
-    public void buyUpgrade() {
-        // TODO Auto-generated method stub
-        
+    public final void setTowerPrice(final String twrName, final Integer price) {
+        twrPrices.put(twrName, price);
+    }
+    /*
+     * check if is possible to buy a specific tower.
+     * @return true if the purchase is successful
+     */
+
+    @Override
+    public boolean hasBoughtTower(final Integer price) {
+        if (wallet.areMoneyEnough(price)) {
+            wallet.subtractMoney(price);
+            return true;
+        }
+        return false;
+    }
+
+    //if is possible to buy the tower then return a new tower which has to be added to the map
+    public final Optional<Tower> buyTowerZone1(final EnemiesPoolInteractor pool, final Pair<Double, Double> pos, final String twrName) {
+        int twrPrice = twrPrices.get(twrName);
+        return hasBoughtTower(twrPrice) ? Optional.of(tf.getTowerZone1(pool, pos)) : Optional.empty();
+    }
+
+    public final Optional<Tower> buyTowerZone2(final EnemiesPoolInteractor pool, final Pair<Double, Double> pos, final String twrName) {
+        int twrPrice = twrPrices.get(twrName);
+        return hasBoughtTower(twrPrice) ? Optional.of(tf.getTowerZone2(pool, pos)) : Optional.empty();
+    }
+
+    public final Optional<Tower> buyTowerDirect1(final EnemiesPoolInteractor pool, final Pair<Double, Double> pos, final String twrName) {
+        int twrPrice = twrPrices.get(twrName);
+        return hasBoughtTower(twrPrice) ? Optional.of(tf.getTowerDirect1(pool, pos)) : Optional.empty();
     }
 
 
 
+    /*
+     * upgrade the level of the towers
+     * */
+    @Override
+    public void buyUpgrade(final Tower tw, final String twName) {
+        Integer upPrice = upgPrices.get(twName);
+        if (wallet.areMoneyEnough(upPrice)) {
+            wallet.subtractMoney(upPrice);
+            tw.upgradeLevel();
+        }
 
-
-	
-
-	
+    }
 
 }
