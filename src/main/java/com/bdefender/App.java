06b727baaf687967b2c7da2960fb54c5da96cade
guariglia <daniel.guariglia@studio.unibo.it>
2021-04-13 12:51:38 +0200

    Added basic Main menu FXML, Loader, Test and controller with EventHandler

--- src/main/java/com/bdefender/App.java
+++ src/main/java/com/bdefender/App.java
@@ -1,14 +1,14 @@
 package com.bdefender;
 
 
-import com.bdefender.test.guariglia.LaunchMenuTest;
+import com.bdefender.test.guariglia.TestMenu;
 import com.bdefender.test.map.MapTest;
 
 
 public class App{
 
     public static void main(String[] args) {
-        MapTest.run(args);
+        TestMenu.run(args);
     }
 
 }
