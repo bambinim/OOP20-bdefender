3ce9ea683f85dcefb2361f8478e2edf5f3e6102d
Matteo Bambini <matteo.bambini00@gmail.com>
2021-04-16 16:30:56 +0200

    Aggiunto top menu a GameView

--- src/main/java/com/bdefender/App.java
+++ src/main/java/com/bdefender/App.java
@@ -1,15 +1,12 @@
 package com.bdefender;
 
 
-import com.bdefender.test.shola.ShopLaunch;
-
-
 public final class App {
 
     private App() { }
 
     public static void main(final String[] args) {
-        ShopLaunch.run(args);
+        AppView.run(args);
     }
 
 }
--- src/main/java/com/bdefender/game/GameView.java
+++ src/main/java/com/bdefender/game/GameView.java
@@ -5,7 +5,18 @@ import com.bdefender.map.MapView;
 
 public class GameView extends AnchorPane {
 
+    private final TopMenuView topMenuView;
+
     public GameView(final MapView mapView) {
-        this.getChildren().add(mapView);
+        this.topMenuView = new TopMenuView();
+        mapView.setLayoutY(TopMenuView.HEIGHT);
+        this.getChildren().addAll(mapView, this.topMenuView);
+    }
+
+    /**
+     * @return top menu view
+     */
+    public TopMenuView getTopMenuView() {
+        return this.topMenuView;
     }
 }
