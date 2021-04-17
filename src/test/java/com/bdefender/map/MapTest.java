fbf408f076604024bdd9de4e5be357846d876bb3
Matteo Bambini <matteo.bambini00@gmail.com>
2021-04-17 11:08:48 +0200

    Aggiunto enemiesPane a MapView e aggiunto test jUnit mappa 1

--- src/test/java/com/bdefender/map/MapTest.java
+++ src/test/java/com/bdefender/map/MapTest.java
@@ -14,7 +14,7 @@ public class MapTest {
         // test path
         assertEquals(map.getPath(), List.of(new Coordinates(0.0, 9.0), new Coordinates(7.0, 9.0)));
         // test tower boxes
-        final List<TowerBox> towerBoxes = new ArrayList<TowerBox>();
+        final List<TowerBox> towerBoxes = new ArrayList<>();
         for (double i = 4 ; i < 8; i+=2) {
             for (double j = 15; j < 22; j+=2) {
                 towerBoxes.add(new TowerBox(new Coordinates(j, i)));
@@ -26,5 +26,15 @@ public class MapTest {
     @Test
     public void loadMapIcePlain() {
         final Map map = MapLoader.getInstance().loadMap(MapType.ICEPLAIN);
+        // test path
+        assertEquals(map.getPath(), List.of(new Coordinates(3.0, 0.0), new Coordinates(3.0, 8.0)));
+        /// test tower boxes
+        final List<TowerBox> towerBoxes = new ArrayList<>();
+        for (double i = 0; i < 9; i+=2) {
+            for (double j = 0; j < 1; j+=2) {
+                towerBoxes.add(new TowerBox(new Coordinates(j, i)));
+            }
+        }
+        assertEquals(map.getTowerBoxes(), towerBoxes);
     }
 }
