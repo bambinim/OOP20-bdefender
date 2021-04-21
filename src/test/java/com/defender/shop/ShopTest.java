659cb5be115ca6a81818adda1f6b024457ab5d1d
oshodiS <shola.oshodi@studio.unibo.it>
2021-04-21 11:42:06 +0200

    Updated Junit Test

--- src/test/java/com/defender/shop/ShopTest.java
+++ src/test/java/com/defender/shop/ShopTest.java
@@ -14,17 +14,18 @@ import com.bdefender.wallet.WalletImpl;
 public class ShopTest {
     @Test
     public void testBuyTower() {
+        
         final int INIT_AMOUNT = 100;
         Wallet wallet = new WalletImpl(INIT_AMOUNT);
         Shop shop = new ShopImpl(wallet);
         shop.buyTower(TowerName.FIRE_ARROW); //price 200
-        assertTrue(shop.getWallet().getMoney() == INIT_AMOUNT);
-        assertFalse(shop.canBuyTower(TowerName.FIRE_ARROW));
+        assertTrue(shop.getWallet().getMoney() == INIT_AMOUNT); //expected no decrease
+        assertFalse(shop.canBuyTower(TowerName.FIRE_ARROW)); //no enough money
         assertTrue(shop.canBuyTower(TowerName.FIRE_BALL)); //price 100
         shop.getWallet().addMoney(300);
-        assertTrue(shop.canBuyTower(TowerName.FIRE_BALL));
+        assertTrue(shop.canBuyTower(TowerName.FIRE_BALL)); //money are enough to buy tower
         shop.buyTower(TowerName.FIRE_BALL);
-        assertTrue(shop.getWallet().getMoney() == 300 );
+        assertTrue(shop.getWallet().getMoney() == 300 ); //expect corret decrease
     }
 
 }
--- src/test/java/com/defender/shop/WalletTest.java
+++ src/test/java/com/defender/shop/WalletTest.java
@@ -13,6 +13,7 @@ import com.bdefender.wallet.WalletImpl;
 public class WalletTest {
     @Test
     public void testWalletAddition() {
+        //check if money is properly increased
        final int INIT_VALUE = 300;
        Wallet wallet = new WalletImpl(INIT_VALUE);
        
@@ -22,9 +23,12 @@ public class WalletTest {
        assertTrue(wallet.getMoney() == 2370);
        wallet.addMoney(0);
        assertTrue(wallet.getMoney() == 2370);
+       wallet.addMoney(30);
+       assertTrue(wallet.getMoney() == 2400);
     }
     @Test
     public void testWalletSubtraction() {
+        //check that money is correctly decrease and that no negative amount are possible
         final int INIT_VALUE = 700;
         Wallet wallet = new WalletImpl(INIT_VALUE);
         
@@ -33,9 +37,9 @@ public class WalletTest {
         wallet.subtractMoney(300);
         assertTrue(wallet.getMoney() == 350);
         wallet.subtractMoney(400);
-        assertFalse(wallet.getMoney() == -50);
-        assertFalse(wallet.areMoneyEnough(900));
-        assertTrue(wallet.areMoneyEnough(350));
+        assertFalse(wallet.getMoney() == -50); //cant't be negative
+        assertFalse(wallet.areMoneyEnough(900)); 
+        assertTrue(wallet.areMoneyEnough(350)); //no decrease should have be done
         wallet.subtractMoney(350);
         assertTrue(wallet.getMoney() == 0);
        
@@ -44,13 +48,22 @@ public class WalletTest {
     }
     @Test
     public void testMixedOperation() {
+        //try addition e subctration together
         final int INIT_VALUE = 0;
         Wallet wallet = new WalletImpl(INIT_VALUE);
         assertFalse(wallet.areMoneyEnough(400));
         wallet.subtractMoney(90);
         assertTrue(wallet.getMoney() == 0);
         wallet.addMoney(60);
-        assertTrue(wallet.getMoney() == 60);
+        wallet.addMoney(300);
+        wallet.addMoney(490);
+        assertTrue(wallet.getMoney() == 850);
+        assertFalse(wallet.areMoneyEnough(851)); //false
+        assertTrue(wallet.areMoneyEnough(850)); //true
+        wallet.subtractMoney(849);
+        wallet.subtractMoney(10); 
+        assertTrue(wallet.getMoney() == 1); //no decrease should have be done
+        
         
     }
     
